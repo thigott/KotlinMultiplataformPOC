12d0334081da2357bdfc9153a022ce1eedf301a9
thiagoalves <thiago.alves@pura.com>
2023-10-19 13:42:06 -0300

    Updating the kmmLibraryVersion

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    id("com.android.application").version("8.1.0-beta02").apply(false)
-    id("com.android.library").version("8.1.0-beta02").apply(false)
+    id("com.android.application").version("8.1.1").apply(false)
+    id("com.android.library").version("8.1.1").apply(false)
     kotlin("android").version("1.8.10").apply(false)
     kotlin("multiplatform").version("1.8.10").apply(false)
 }
